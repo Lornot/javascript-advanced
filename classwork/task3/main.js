47e3b6fe041377279269723f298ba73139cd7112
Oleg <lornot@meta.ua>
2018-02-12 20:00:29 +0200

    added new button

--- classwork/task3/main.js
+++ classwork/task3/main.js
@@ -1,3 +1,6 @@
 (function () {
-    console.log('main branch');
-}())
+    console.log('new button branch');
+    var button = document.createElement('button');
+    btn.innerHTML = 'New button';
+    document.body.appendChild(button);
+}());
